c0afbf96b19e28025d039d80e4d8fa51cdc2873a
brianruhiu <116198613+BrianRuhiuMuhia@users.noreply.github.com>
2024-03-04 18:49:37 +0300

    ServeHTTP method

--- rest-api/main.go
+++ rest-api/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 type Home struct {
@@ -14,17 +15,49 @@ type Recipie struct {
 func (home Home) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Recipies HomePage")
 }
-func (recipie Recipie) CreateRecipie(w http.ResponseWriter, r *http.Response) {
+func (recipie Recipie) CreateRecipie(w http.ResponseWriter, r *http.Request) {
 
 }
-func (recipie Recipie) DeleteRecipie(w http.ResponseWriter, r *http.Response) {
+func (recipie Recipie) DeleteRecipie(w http.ResponseWriter, r *http.Request) {
 
 }
-func (recipie Recipie) AddRecipie(w http.ResponseWriter, r *http.Response) {
+func (recipie Recipie) AddRecipie(w http.ResponseWriter, r *http.Request) {
 
 }
-func (recipie Recipie) UpdateRecipie(w http.ResponseWriter, r *http.Response) {
+func (recipie Recipie) UpdateRecipie(w http.ResponseWriter, r *http.Request) {
 
+}
+func (recipie Recipie) getRecipie(w http.ResponseWriter, r *http.Request) {
+
+}
+func (recipie Recipie) allRecipie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+var (
+	RecipeRe       = regexp.MustCompile(`^/recipes/*$`)
+	RecipeReWithID = regexp.MustCompile(`^/recipes/([a-z0-9]+(?:-[a-z0-9]+)+)$`)
+)
+
+func (recipie Recipie) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch {
+	case r.Method == http.MethodPost && RecipeRe.MatchString(r.URL.Path):
+		recipie.CreateRecipie(w, r)
+		return
+	case r.Method == http.MethodGet && RecipeReWithID.MatchString(r.URL.Path):
+		recipie.getRecipie(w, r)
+		return
+	case r.Method == http.MethodGet && RecipeRe.MatchString(r.URL.Path):
+		recipie.allRecipie(w, r)
+		return
+	case r.Method == http.MethodPut && RecipeReWithID.MatchString(r.URL.Path):
+		recipie.UpdateRecipie(w, r)
+		return
+	case r.Method == http.MethodDelete && RecipeReWithID.MatchString(r.URL.Path):
+		recipie.DeleteRecipie(w, r)
+		return
+
+	}
 }
 func main() {
 	mux := http.NewServeMux()
